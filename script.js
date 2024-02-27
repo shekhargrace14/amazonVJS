47a54a1513df142c07dd19ca88c19e191fa51e5e
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-02-27 12:07:10 +0000

    Design  changed

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 fetch("https://fakestoreapi.com/products")
 .then(response=>response.json())
 .then(data=>{
-    console.log(data);
+    // console.log(data);
     let productsRow = document.querySelector(".productsRow");
     data.map((value,index)=>{
         productsRow.insertAdjacentHTML("beforeend",`
-        <a href="./singleProductPage.html">
-            <div class="column card">
-                <div class="imgBox">
-                    <img src=${value.image} alt="">
-                </div>
-                <div class="info">
-                    <p class="productTitle">${value.title}</p>
-                    <p class="description"> ${value.description}</p>
-                    <div class="price d-flex ">
-                        <p class="discount">$ ${value.price + 6}</p>
-                        <p class="CurrentPrice">$ ${value.price}</p>
+        <div class="column card">
+            <a href="./singleProductPage.html" id=${value.id}>
+                <div class="column cardBody">
+                    <div class="imgBox">
+                        <img src=${value.image} alt="">
                     </div>
-                    <div class="buttonDiv">
-                        <button class="btn-primary">Add to cart</button>
-                    </div>
+                    <div class="info">
+                        <p class="productTitle">${value.title}</p>
+                        <p class="description"> ${value.description}</p>
+                        <div class="price d-flex ">
+                            <p class="discount">$ ${value.price + 6}</p>
+                            <p class="CurrentPrice">$ ${value.price}</p>
+                        </div>
+                        <div class="buttonDiv">
+                            <button class="btn-primary">Add to cart</button>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
         `)
         // console.log(value.title)
+    })
+    let card = document.querySelectorAll(".card")
+    console.log(card, "card");
+    card.forEach((item,index)=>{
+        item.addEventListener("click", ()=>{
+            let itemID = item.firstElementChild.id
+            console.log(itemID, "card has been clicked")
+            localStorage.setItem("singleProductPage",itemID)
+        })
     })
 })
---
+++ singleProductPage.js
@@ -0,0 +1,18 @@
+fetch("https://fakestoreapi.com/products")
+.then(response=>response.json())
+.then(data=>{
+    let clickedId = localStorage.getItem("singleProductPage")
+    let matchedId = data.find((item)=>{
+        return item.id.toString() === clickedId;
+    });
+    console.log( matchedId,"this is matchedId")
+    
+    if(matchedId){
+        // document.querySelector("#sc").innerHTML = matchedId.image;  
+        document.querySelector(".productTitle").innerHTML = matchedId.title;
+        document.querySelector(".description").innerHTML = matchedId.description;
+        document.querySelector(".discount").innerHTML = matchedId.price + 5;
+        document.querySelector(".CurrentPrice").innerHTML = matchedId.price;
+        document.getElementById("imageUrl").src = matchedId.image;
+    }
+})
