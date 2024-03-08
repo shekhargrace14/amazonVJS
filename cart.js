8406a4e795a2406850fc32c4d3a9d7c3eab2883d
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-03-08 04:41:09 +0000

    xbv

--- cart.js
+++ cart.js
@@ -5,16 +5,14 @@ fetch("https://fakestoreapi.com/products")
 .then(response=>response.json())
 .then(data=>{
     console.log(data);
-    
     let addToCartItems = JSON.parse(localStorage.getItem("addToCartItems"))
     console.log(addToCartItems);
     let addToCartMatchedItems = data.filter((element)=>{
         return addToCartItems.includes(element.id.toString())
     });
-    console.log(addToCartMatchedItems, "addToCartMatchedItems")
-
+    // console.log(addToCartMatchedItems, "addToCartMatchedItems")
     let table = document.querySelector("table");
-    console.log(table)
+    // console.log(table)
     addToCartMatchedItems.map((element)=>{
         table.insertAdjacentHTML("beforeend",`
             <tr>
@@ -25,12 +23,28 @@ fetch("https://fakestoreapi.com/products")
                     <p>${element.title}</p>
                 </td>
                 <td>1</td>
-                <td>${element.price}</td>
+                <td class="price">${element.price}</td>
             </tr>
-
-
-        `)
-        
+        `)      
     })
 
+
+
+    let price = document.querySelectorAll(".price")
+    let priceValue = price.innerHTML
+    console.log(priceValue, "priceValue");
+    // let price = [1,2,3,4,5,6,7,8,9,0]
+    let totalPriceCalculation = price.forEach((element)=>{
+        let priceSingle = element.innerHTML;
+        // console.log(priceSingle, "priceSingle");
+
+        let grandTotal = 0;
+
+        grandTotal = grandTotal + Number(priceSingle)
+
+        // console.log(grandTotal, "grandTotal");
+
+
+    })
+    console.log(totalPriceCalculation, "totalPriceCalculation");
 })
