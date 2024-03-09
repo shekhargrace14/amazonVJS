9ccff35b90aa2a45fe786fa4f7cbab2fded58e0b
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-03-09 04:29:13 +0000

    quantity increase decrease added

--- cart.js
+++ cart.js
@@ -1,12 +1,10 @@
-
 // console.log(JSON.parse(localStorage.getItem("addToCartItems")))
-
 fetch("https://fakestoreapi.com/products")
 .then(response=>response.json())
 .then(data=>{
-    console.log(data);
+    // console.log(data);
     let addToCartItems = JSON.parse(localStorage.getItem("addToCartItems"))
-    console.log(addToCartItems);
+    // console.log(addToCartItems);
     let addToCartMatchedItems = data.filter((element)=>{
         return addToCartItems.includes(element.id.toString())
     });
@@ -17,28 +15,87 @@ fetch("https://fakestoreapi.com/products")
     addToCartMatchedItems.map((element)=>{
         totalPriceArray.push(element.price)
         table.insertAdjacentHTML("beforeend",`
-            <tr>
+            <tr class="product">
                 <td>
                     <img src=${element.image} alt="" />
                 </td>
                 <td>
                     <p>${element.title}</p>
                 </td>
-                <td>1</td>
+                <td class="quantity">
+                    <button class="decrement" >-</button>
+                    <span class="value">1</span>
+                    <button class="increment" >+</button>
+                </td>
                 <td class="price">${element.price.toFixed(2)}</td>
             </tr>
-        `)      
+        `)    
     })
-    let totalPrice = totalPriceArray.reduce((total, price)=>price+total,0)
-    console.log(totalPrice,"totalPrice")
-    let grandTotal = document.querySelector(".grandTotal")
-    console.log(grandTotal,"grandTotal")
 
+    
+    // increment decrement start 
+    let increment = document.querySelectorAll(".increment");
+    console.log(increment)
+
+    let value = document.querySelectorAll(".value");
+    console.log(value)
+
+    
+
+    
+    
+    
+    increment.forEach((element,index)=>{
+
+        element.addEventListener("click",()=>{
+            let initValue = value[index].innerHTML; 
+            console.log(initValue,"initValue")
+            initValue++;
+            value[index].innerHTML = initValue;
+            // console.log(initValue,"initValue")
+            // console.log(index,"element quantity")
+
+        })      
+    })
+
+    let decrement = document.querySelectorAll(".decrement");
+    console.log(decrement)
+
+    decrement.forEach((element,index)=>{
+
+        element.addEventListener("click",()=>{
+            let initValue = value[index].innerHTML; 
+            console.log(initValue,"initValue")
+            if(initValue == 0 ){
+                return
+            }else{
+
+                initValue--;
+                value[index].innerHTML = initValue;
+            }
+            // console.log(initValue,"initValue")
+            // console.log(index,"element quantity")
+
+        })      
+    })
+    // increment decrement ends   
+
+    
+
+    // grandTotal starts here 
+    let totalPrice = totalPriceArray.reduce((total, price)=>price+total,0)
+    // console.log(totalPrice,"totalPrice")
+    let grandTotal = document.querySelector(".grandTotal")
+    // console.log(grandTotal,"grandTotal")
     grandTotal.insertAdjacentHTML("beforeend",`
         <tr>
             <td>Total</td>
             <td class="totalPRice">${totalPrice.toFixed(2)}</td>
-        </tr>
-    
+        </tr> 
     `)  
+    // grandTotal ends here 
+    
 })
+
+
+
