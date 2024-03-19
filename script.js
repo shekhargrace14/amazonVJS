922762f1a36ab0c75f59f675e01d9185395ad335
shekhargrace14 <82882207+shekhargrace14@users.noreply.github.com>
2024-03-19 12:55:16 +0530

    major changes on home page

--- script.js
+++ script.js
@@ -9,23 +9,32 @@ fetch("https://fakestoreapi.com/products")
     data.map((value,index)=>{
         productsRow.insertAdjacentHTML("beforeend",`
         <div class="column card">
-            <a href="./singleProductPage.html" id=${value.id}>
-                <div class="column cardBody">
+                <div class="column cardBody" id=${value.id}>
+                    <div class="utilityIcons d-flex direction-column">
+                        <i class="fa-regular fa-heart"></i>
+                        <i class=" onHover fa-solid fa-magnifying-glass"></i>
+                    </div>
                     <div class="imgBox">
                         <img src=${value.image} alt="">
-                    </div>
+
+                    </div>                   
                     <div class="info">
-                        <p class="productTitle">${value.title}</p>
-                        <p class="description"> ${value.description}</p>
+                        <p class="rating"> ${value.rating.rate} | ${value.rating.count} </p>
+                        <h3 class="productTitle">${value.title}</h3>
+                        
                         <div class="price d-flex ">
-                            <p class="discount">$ ${(value.price+ 5).toFixed(2) }</p>
-                            <p class="CurrentPrice">$ ${value.price.toFixed(2)}</p>
+                        <p class="discount">$ ${(value.price+ 10).toFixed(2) }</p>
+                        <p class="CurrentPrice">$ ${value.price.toFixed(2)}</p>
                         </div>
-
+                    </div>
+                    <div >
+                        <a href="./singleProductPage.html" id=${value.id}>
+                            <button class="btn-primary" >Quick Shop</button>
+                        </a>
 
                     </div>
+
                 </div>
-            </a>
         </div>
         `)
         // console.log(value.title)
