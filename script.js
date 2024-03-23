847f6a79239f4cc0ebb78ae5b9963c6d8a8acb14
shekhargrace14 <shekhargrace14@gmail.com>
2024-03-23 16:50:01 +0530

    filter side bar added

--- script.js
+++ script.js
@@ -1,57 +1,84 @@
-// categories 1. 
-
-let header = document.querySelector("header")
 
 
-fetch("https://fakestoreapi.com/products")
-.then(response=>response.json())
-.then(data=>{
-    console.log(data);
-    let productsRow = document.querySelector(".productsRow");
-    data.map((value,index)=>{
-        productsRow.insertAdjacentHTML("beforeend",`
-        <div class="column card">
-                <div class="column cardBody" id=${value.id}>
-                    <div class="utilityIcons d-flex direction-column">
-                        <i class="fa-regular fa-heart"></i>
-                        <i class=" onHover fa-solid fa-magnifying-glass"></i>
-                    </div>
-                    <div class="imgBox">
-                        <img src=${value.image} alt="">
+// product filter stats here 
+let productsRow = document.querySelector(".productsRow");
+let category =  document.querySelector(".FilterCategory")
+let allCat = [];
 
-                    </div>                   
-                    <div class="info">
-                        <p class="rating"> ${value.rating.rate} | ${value.rating.count} </p>
-                        <h3 class="productTitle">${value.title}</h3>
-                        
-                        <div class="price d-flex ">
-                        <p class="CurrentPrice">$ ${value.price.toFixed(2)}</p>
-                        <p class="discount">$ ${(value.price+ 10).toFixed(2) }</p>
+let displayProduct = async(allCheckCat=[])=>{
+    console.log(allCheckCat)
+    productsRow.innerHTML = ''
+    let product = await fetch("https://fakestoreapi.com/products");
+    let finalProduct = await product.json();
+    finalProduct.forEach((element)=>{
+        if(!allCat.includes(element.category)){
+            category.innerHTML+=`
+            <input type="checkbox" onclick='categoryFilter()' value="${element.category}">${element.category}</input> <br>
+            `
+            allCat.push(element.category)
+            // <input type="checkbox" onclick='categoryFilter("${element.category}")' value="${element.category}">${element.category}</input> <br>
+        }
+        if(allCheckCat.length==0){
+            allCheckCat = allCat;
+            console.log(allCheckCat)
+
+        }
+
+        if(allCheckCat.includes(element.category)){
+            productsRow.innerHTML+=`
+                <div class="column card">
+                    <a href="./singleProductPage.html" id=${element.id}>
+                        <div class="column cardBody">
+                            <div class="imgBox">
+                                <img src=${element.image} alt="">
+                            </div>
+                            <div class="info">
+                                <p class="productTitle">${element.title}</p>
+                                <p class="productCategory">${element.category}</p>
+                                <p class="description"> ${element.description}</p>
+                                <div class="price d-flex ">
+                                    <p class="discount">$ ${(element.price+ 5).toFixed(2) }</p>
+                                    <p class="CurrentPrice">$ ${element.price.toFixed(2)}</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div >
-                        <a href="./singleProductPage.html" id=${value.id}>
-                            <button class="btn-primary" >Quick Shop</button>
-                        </a>
-
-                    </div>
-
+                    </a>
                 </div>
-        </div>
-        `)
-        // console.log(value.title)
-    })
-    let card = document.querySelectorAll(".card")
-    console.log(card, "card");
-    card.forEach((item,index)=>{
+                `
+                // console.log(value.title)
+        }
+
+        let card = document.querySelectorAll(".card")
+        console.log(card, "card");
+        card.forEach((item,index)=>{
         item.addEventListener("click", ()=>{
             let itemID = item.firstElementChild.id
             console.log(itemID, "card has been clicked")
             localStorage.setItem("singleProductPage",itemID)
         })
     })
-})
 
+    })
+   
+}
+
+displayProduct()
+
+let categoryFilter=()=>{
+    let checkInput = document.querySelectorAll("input[type='checkbox']")
+    let checkData = [];
+    checkInput.forEach((e)=>{
+        if(e.checked){
+            checkData.push(e.value);
+            // console.log(e.value)
+        }
+    })
+    displayProduct(checkData)
+    // console.log(checkData)
+// /
+
+}
+// product filter ends here 
 
 // productSlider start here 
 
@@ -59,9 +86,9 @@ fetch("https://fakestoreapi.com/products")
 fetch("./json/productApiLocal.json")
 .then((resolve)=> resolve.json())
 .then((data)=>{
-    console.log(data)
+    // console.log(data)
     let carousel = document.querySelector(".productSliderCarousel")
-    console.log(carousel)
+    // console.log(carousel)
     data.map((value)=>{
         carousel.insertAdjacentHTML("beforeend",`
             <div class="column productSliderCard">
@@ -94,15 +121,15 @@ fetch("./json/productApiLocal.json")
     let cardWidth = document.querySelector(".productSliderCard").getBoundingClientRect().width
     // const cardWidth = card.getBoundingClientRect()
     // let cardWidth = card.offsetWidth;
-    console.log(productSliderCard)
-    console.log(cardWidth)
+    // console.log(productSliderCard)
+    // console.log(cardWidth)
 
     let arrows = document.querySelectorAll(".arrow")
 
     arrows.forEach((btn)=>{
-        console.log(btn)
+        // console.log(btn)
         btn.addEventListener("click",()=>{
-            console.log(btn.id)
+            // console.log(btn.id)
             carousel.scrollLeft += btn.id === "left" ? -cardWidth -8 : + cardWidth +8;
 
         })
@@ -113,3 +140,7 @@ fetch("./json/productApiLocal.json")
 })
 
 // productSlider ends here 
+
+// product filter starts
+// let  
+// product filter ends
--- women.js
+++ women.js
@@ -1,17 +1,60 @@
-
-let productDiv = document.querySelector(".productRow");
-
-let displayProduct= async ()=>{
-    // productDiv.innerHTML= '';
+let productsRow = document.querySelector(".productsRow");
+let category =  document.querySelector(".category")
+console.log(category)
+let allCat = [];
+       
+let displayProduct= async (allCheckCat = [])=>{
+    productsRow.innerHTML= '';
     let product = await fetch("https://fakestoreapi.com/products")
     let finalProduct = await product.json();
-        console.log(value)
+    finalProduct.forEach((element) => {
+        if(!allCat.includes(element.category)){
+            category.innerHTML+=`
+            <input type="checkbox" onclick='categoryFilter()' value="${element.category}">${element.category}</input> <br>`
 
+            allCat.push(element.category)  
+        }
+        if(allCheckCat.length==0){
+            allCheckCat=allCat
+        }
+        if(allCheckCat.includes(element.category)){
+            productsRow.innerHTML+=`
+            <div class="column card">
+            <a href="./singleProductPage.html" id=${element.id}>
+                <div class="column cardBody">
+                    <div class="imgBox">
+                        <img src=${element.image} alt="">
+                    </div>
+                    <div class="info">
+                        <p class="productTitle">${element.title}</p>
+                        <p class="productCategory">${element.category}</p>
+                        <p class="description"> ${element.description}</p>
+                        <div class="price d-flex ">
+                            <p class="discount">$ ${(element.price+ 5).toFixed(2) }</p>
+                            <p class="CurrentPrice">$ ${element.price.toFixed(2)}</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            </div>`
 
-    finalProduct.forEach((value) => {
-        // console.log(value)
-
+        }
     });
+
 }
 
 displayProduct()
+
+let categoryFilter=()=>{
+    let checkInput=document.querySelectorAll("input[type='checkbox']");
+    let checkData = [];
+    checkInput.forEach((e)=>{
+        if(e.checked){
+            checkData.push(e.value)
+
+            // console.log(checkData)
+        }   
+    })
+    displayProduct(checkData)
+
+}
